7364ffb6cab4694ae29040c2c24f1cb45d85dc6a
Saro <112953065+Saravanangrizz@users.noreply.github.com>
2025-04-18 20:52:59 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -2,7 +2,7 @@ import socket
 from ipaddress import ip_network
 from concurrent.futures import ThreadPoolExecutor
 
-# Ports to scan — you can customize this
+
 COMMON_PORTS = [21, 22, 23, 25, 53, 80, 110, 139, 143, 443, 445, 3389]
 
 def scan_host(ip, ports=COMMON_PORTS):
@@ -17,15 +17,22 @@ def scan_host(ip, ports=COMMON_PORTS):
 
 def scan_network(network):
     results = []
+
     try:
         ip_net = ip_network(network, strict=False)
+        ip_list = [str(ip) for ip in ip_net.hosts()]
     except ValueError:
-        return []
+        try:
+            ip = socket.gethostbyname(network)
+            ip_list = [ip]
+        except socket.gaierror:
+            return []
 
     with ThreadPoolExecutor(max_workers=50) as executor:
-        futures = [executor.submit(scan_host, str(ip)) for ip in ip_net.hosts()]
+        futures = [executor.submit(scan_host, ip) for ip in ip_list]
         for future in futures:
             ip, open_ports = future.result()
             if open_ports:
                 results.append((ip, open_ports))
     return results
+
